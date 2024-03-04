f0318ea16f069f9fa862e6958fc62e5e074988fe
shrinidhi-a <shrinidhisringeri@gmail.com>
2024-03-04 15:02:05 +0530

    Controllers basic

--- nest-js-first/src/app.controller.ts
+++ nest-js-first/src/app.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Post, Req, Param, Delete, Patch } from "@nestjs/common";
 
-@Controller()
+@Controller("/user")
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    // /user/{userId}
+    @Get("{userId}")
+    getUsers() {
+        return { name: "John Doe", age: 25, email: "john@john.com" };
+    }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Post("")
+    store(@Req() req: Request) {
+        console.log(req.body);
+        return req.body;
+    }
+
+    @Get("/:userId")
+    getUser(@Param() params: { userId: number }) {
+        return params;
+    }
+
+    @Delete("/:userId")
+    deleteUser(@Param() params: { userId: number }) {
+        return params;
+    }
+
+    @Patch("/:userId")
+    updateUser(@Param() params: { userId: number }) {
+        return params;
+    }
 }
